4bcc56fc7612e5e6a0fa9a8035293373c47dbcea
Huss-Sahin <62580034+Huss-Sahin@users.noreply.github.com>
2020-06-25 22:31:14 +1000

    Create websysytems.css

---
+++ css/websysytems.css
@@ -0,0 +1,182 @@
+p {
+  text-align: left;
+}
+
+
+.menuicon a {
+  text-decoration: none;
+}
+
+.header{
+  padding: 20px;
+  margin: 0;
+  margin-bottom: 0px;
+  padding: 0;
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
+  display: flex;
+  min-height: 100vh;
+  flex-direction:column;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.header img{
+width: 100%;
+height: 240px;
+display: flex;
+object-fit: cover;
+}
+
+
+
+.picture{
+  text-align: center;
+  margin-bottom: 60px;
+  width:20%;
+  height:10%;  
+}
+
+.responsive{
+  text-align: center;
+  margin-bottom: 100px;
+  max-width: 80%;
+ 
+}
+
+.menuicon {
+  margin-top: 0px;
+  background-color: #0a0f28;
+  padding: 11px 11px;
+  text-align: center;
+  word-spacing: 80px;
+  color: white;
+}
+
+h1{
+    display: block;
+    font-size: 1em;
+    font-weight: bold;
+    text-align: center;
+}
+
+.bg {
+  background-color: #044A61;
+  flex: 1 0 auto;
+}
+
+.content {
+  /* width: 80%;
+  text-align: center;
+  margin: 0 auto; */
+  max-width: 1140px;  
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  text-align: center;
+
+}
+
+
+/*.content ul{
+  text-align: center;
+  list-style: inside;
+  display:inline;  
+}*/
+
+.listNew {text-align: center;}
+
+.listNew > ul {
+  display: inline-block;
+}
+
+.listNew li {
+  margin-bottom: 20px;
+  text-align: left;
+}
+.listNew li:last-child {
+  margin-bottom: 0;
+}
+
+/*
+li{
+    min-height : 45px;
+    vertical-align: middle;  
+}
+*/
+
+h2 {
+  display: block;
+  text-align: center;
+  font-size: 1.5em;
+  font-weight: bold;
+  color: #fff;
+}
+
+#a{
+  display: block;
+  text-align: center;
+  color: #fff;
+
+}
+
+webkit-any-link {
+  color: white;
+  cursor: pointer;
+}
+
+.footer {
+  margin-bottom: 0px;
+  left: 0;
+  right: 0;
+  background-image:url(header.jpg);
+  color: white;
+  text-align: center;
+  height: 40px; 
+  padding:15px;
+  
+}
+a{
+  color:white
+}
+
+.textbox {
+  
+  text-align: center;
+  /* padding-top: 4px;
+  padding-bottom: 4px;
+  padding-left: 4px;
+  padding-right: 4px;
+  margin-left: 19%;
+  margin-right: 19%;
+  margin-bottom: 50px;
+  margin-top: 50px; */
+  color: white !important;
+}
+
+.schema{
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 75px;
+  width: 45%;
+  height:45%;
+  
+}
+
+html{
+height: 100%;
+}
+
+table, th, td{
+  text-align: left;
+  border: 1px solid white;
+  margin: 0 auto;
+}
+
+a:visited{
+color:white;
+}
